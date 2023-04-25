33c74684e007ba1ebe2386d3b71f9a7e4b7102d1
Nitesh <niteshnagar1142002@gmail.com>
2023-04-25 11:00:07 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,27 @@
+const { render } = require('ejs');
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  var filesDup = [];
+  fs.readdir('./upload', { withFileTypes: true }, function (err, files) {
+    // console.log(files);
+    files.forEach(function (file) {
+      filesDup.push({ fileName: file.name, isFolder: file.isDirectory() });
+    });
+    res.render('vsCode', { files: filesDup });
+  });
 });
 //  visual studio route
 router.get('/vsCode', function (req, res) {
   var filesDup = [];
   fs.readdir('./upload', { withFileTypes: true }, function (err, files) {
     // console.log(files);
-    files.forEach(function(file){
-      filesDup.push({fileName:file.name,isFolder:file.isDirectory()});
+    files.forEach(function (file) {
+      filesDup.push({ fileName: file.name, isFolder: file.isDirectory() });
     });
     res.render('vsCode', { files: filesDup });
   });
@@ -23,10 +32,10 @@ router.post('/fileCreate', function (req, res) {
   fs.writeFileSync('./upload/' + req.body.fileName, '');
   fs.readdir('./upload', { withFileTypes: true }, function (err, files) {
     console.log(files);
-    files.forEach(function(file){
-      filesDup.push({fileName:file.name,isFolder:file.isDirectory()});
+    files.forEach(function (file) {
+      filesDup.push({ fileName: file.name, isFolder: file.isDirectory() });
     });
-    res.render('vsCode', { file: filesDup });
+    res.render('vsCode', { files: filesDup });
   });
 });
 //  form with folder create route 
@@ -34,22 +43,32 @@ router.post('/folderCreate', function (req, res) {
   var filesDup = [];
   fs.mkdirSync('./upload/' + req.body.folderName);
   fs.readdir('./upload', { withFileTypes: true }, function (err, files) {
-    files.forEach(function(file){
-      filesDup.push({fileName:file.name,isFolder:file.isDirectory()});
+    files.forEach(function (file) {
+      filesDup.push({ fileName: file.name, isFolder: file.isDirectory() });
     });
     res.render('vsCode', { files: filesDup });
   });
 });
 
-router.get('/upload/:filename',function(req,res){
+router.get('/upload/:filename', function (req, res) {
   var filesDup = [];
   fs.readdir('./upload', { withFileTypes: true }, function (err, files) {
-    files.forEach(function(file){
-      filesDup.push({fileName:file.name,isFolder:file.isDirectory()});
+    files.forEach(function (file) {
+      filesDup.push({ fileName: file.name, isFolder: file.isDirectory() });
     });
-    fs.readFile(`./upload/${req.params.filename}`,"utf8",function(err,data){
-      res.render('openfile', { files: filesDup,openFileName:req.params.filename,data:data});
+    fs.readFile(`./upload/${req.params.filename}`, "utf8", function (err, data) {
+      res.render('openfile', { files: filesDup, openFileName: req.params.filename, data: data });
     });
   });
 })
+
+router.get('/delete/:filename', function (req, res) {
+  fs.unlink(`./upload/${req.params.filename}`, function (err) {
+    if (!err) {
+      console.log('file deleted....');
+      res.redirect('/');
+    }
+  });
+});
+
 module.exports = router;
